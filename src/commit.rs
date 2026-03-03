e111fc69655463667f18b398691e32c10ef454a2
Josh Matthews <josh@joshmatthews.net>
2026-03-03 03:11:20 -0500

    Initial commit

---
+++ src/commit.rs
@@ -0,0 +1,183 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+#[derive(Debug, Default, PartialEq)]
+pub enum State {
+    #[default]
+    Untriaged,
+    Ignored,
+    Accepted,
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub struct Commit {
+    pub url: String,
+    pub authors: Vec<String>,
+    pub title: String,
+    pub body: Vec<String>,
+    pub date: String,
+    pub state: State,
+    pub label: String,
+}
+
+pub fn write_to_file(commits: Vec<Commit>, path: &Path) -> Result<(), ()> {
+    let contents = std::fs::read_to_string(path).map_err(|_| ())?;
+    let mut updated = String::new();
+    let mut index = 0;
+    let mut save_next_label: Option<String> = None;
+    for line in contents.lines() {
+        if let Some(label) = save_next_label.take() {
+            updated.push_str("    ");
+            updated.push_str(&label);
+            updated.push('\n');
+            if !line.starts_with("    #") {
+                continue;
+            }
+        }
+        if !line.starts_with("https") &&
+            !line.starts_with("-") &&
+            !line.starts_with("+")
+        {
+            updated.push_str(line);
+            updated.push('\n');
+            continue;
+        }
+        let rest = line.strip_prefix("-")
+            .or_else(|| line.strip_prefix("+"))
+            .unwrap_or(line)
+            .to_owned();
+        assert!(rest.starts_with(&commits[index].url));
+        match commits[index].state {
+            State::Ignored => updated.push('-'),
+            State::Accepted => updated.push('+'),
+            State::Untriaged => {},
+        }
+        updated.push_str(&rest);
+        updated.push('\n');
+        save_next_label = if !commits[index].label.is_empty() {
+            Some(commits[index].label.clone())
+        } else {
+            None
+        };
+        index += 1;
+    }
+
+    let mut buffer = File::create(path).map_err(|_| ())?;
+    buffer.write_all(updated.as_bytes()).map_err(|_| ())?;
+    Ok(())    
+}
+
+pub fn parse_from_file(path: &Path) -> Result<Vec<Commit>, ()> {
+    let contents = std::fs::read_to_string(path).map_err(|_| ())?;
+    Ok(parse_from_str(&contents))
+}
+
+fn parse_from_str(contents: &str) -> Vec<Commit> {
+    let mut commits = vec![];
+    let mut commit = Commit::default();
+    let mut current_date = String::new();
+    for line in contents.lines() {
+        if line.starts_with(">>>") {
+            current_date = line.strip_prefix(">>> ").unwrap().to_owned();
+            continue;
+        }
+        if let Some(rest) = line.strip_prefix("    # ") {
+            commit.body.push(rest.to_owned());
+            continue;
+        }
+        if let Some(rest) = line.strip_prefix("    ") {
+            commit.label = rest.to_owned();
+            continue;
+        }
+        if commit != Commit::default() {
+            commits.push(commit);
+            commit = Commit::default();
+        }
+        commit.date = current_date.clone();
+        let line_rest;
+        if let Some(rest) = line.strip_prefix("-") {
+            commit.state = State::Ignored;
+            line_rest = rest;
+        } else if let Some(rest) = line.strip_prefix("+") {
+            commit.state = State::Accepted;
+            line_rest = rest;
+        } else {
+            line_rest = line;
+        }
+        let mut parts = line_rest.split("\t");
+        commit.url = parts.next().unwrap().to_owned();
+        let author_info = parts.next().unwrap();
+        let author_info = author_info.strip_prefix("(").unwrap().strip_suffix(")").unwrap();
+        let mut author_info = author_info.split(",").map(|part| part.trim().to_owned()).collect::<Vec<_>>();
+        author_info.pop();
+        commit.authors = author_info;
+        if commit.authors.contains(&"@dependabot[bot]".to_owned()) ||
+            commit.authors.contains(&"@servo-wpt-sync".to_owned())
+        {
+            commit.state = State::Ignored;
+        }
+        commit.title = parts.next().unwrap().to_owned();
+    }
+    commits.push(commit);
+    commits
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let contents = r#">>> 2026-01-01T06:05:47Z
++https://github.com/servo/servo/pull/41604	(@kkoyung, #41604)	script: Implement export key operation of ML-KEM (#41604)
+    dom; web crypto
+    # Continue on adding ML-KEM support to WebCrypto API.  Specification:
+    # https://wicg.github.io/webcrypto-modern-algos/#ml-kem
+    # This patch implements export key operation of ML-KEM, with `ml-kem` crate.
+    # Testing: Pass some WPT tests that were expected to fail.  Fixes: Part of #41473
+-https://github.com/servo/servo/pull/41198	(@Narfinger, #41198)	Base: Rename IpcSharedMemory to GenericSharedMemory (#41198)
+    # In the future, servo components should depend on the generic channels in base instead of IpcChannels to correctly
+    # optimize for multiprocess vs non-multiprocess mode.  This reexports IpcSharedMemory as GenericSharedMemory in
+    # GenericChannel and changes all dependencies on it.
+    # Currently this is only a type/name change and does not change functionality.  But in the future we would want want to
+    # use non-ipc things for the data.
+    # Signed-off-by: Narfinger
+    # Testing: This changes types and does not need testing."#;
+        let commits = parse_from_str(contents);
+        let expected = vec![
+            Commit {
+                url: "https://github.com/servo/servo/pull/41604".to_owned(),
+                authors: vec!["@kkoyung".to_owned()],
+                title: "script: Implement export key operation of ML-KEM (#41604)".to_owned(),
+                body: vec![
+                    "Continue on adding ML-KEM support to WebCrypto API.  Specification:".to_owned(),
+                    "https://wicg.github.io/webcrypto-modern-algos/#ml-kem".to_owned(),
+                    "This patch implements export key operation of ML-KEM, with `ml-kem` crate.".to_owned(),
+                    "Testing: Pass some WPT tests that were expected to fail.  Fixes: Part of #41473".to_owned(),
+                ],
+                date: "2026-01-01T06:05:47Z".to_owned(),
+                label: "dom; web crypto".to_owned(),
+                state: State::Accepted,
+            },
+            Commit {
+                url: "https://github.com/servo/servo/pull/41198".to_owned(),
+                authors: vec!["@Narfinger".to_owned()],
+                title: "Base: Rename IpcSharedMemory to GenericSharedMemory (#41198)".to_owned(),
+                body: vec![
+                    "In the future, servo components should depend on the generic channels in base instead of IpcChannels to correctly".to_owned(),
+                    "optimize for multiprocess vs non-multiprocess mode.  This reexports IpcSharedMemory as GenericSharedMemory in".to_owned(),
+                    "GenericChannel and changes all dependencies on it.".to_owned(),
+                    "Currently this is only a type/name change and does not change functionality.  But in the future we would want want to".to_owned(),
+                    "use non-ipc things for the data.".to_owned(),
+                    "Signed-off-by: Narfinger".to_owned(),
+                    "Testing: This changes types and does not need testing.".to_owned(),
+                ],
+                date: "2026-01-01T06:05:47Z".to_owned(),
+                label: String::new(),
+                state: State::Ignored,
+            }
+        ];
+        assert_eq!(commits, expected);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,307 @@
+//use ratatui::{DefaultTerminal, Frame};
+use std::cell::Cell;
+use std::env;
+use std::io;
+use std::path::PathBuf;
+use std::process::Command;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Position, Rect, Constraint},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget, Padding},
+    DefaultTerminal, Frame,
+};
+
+use crate::commit::{Commit, State, parse_from_file, write_to_file};
+
+mod commit;
+
+#[derive(Debug)]
+pub struct App {
+    commits: Vec<Commit>,
+    index: usize,
+    unroll: bool,
+    exit: bool,
+    edit_tag: bool,
+    /// Current value of the input box
+    input: String,
+    /// Position of cursor in the editor area.
+    character_index: usize,
+    cursor_pos: Cell<Option<(u16, u16)>>,
+}
+
+fn main() {
+    let mut args = env::args();
+    args.next();
+    let path = PathBuf::from(args.next().unwrap());
+    let commits = parse_from_file(&path).unwrap(); 
+    let mut app = App {
+        index: commits.iter().position(|commit| commit.state == State::Untriaged).unwrap_or(0),
+        commits,
+        edit_tag: false,
+        unroll: false,
+        exit: false,
+        input: String::new(),
+        character_index: 0,
+        cursor_pos: Cell::new(None),
+    };
+    ratatui::run(|terminal| app.run(terminal)).unwrap();
+
+    write_to_file(app.commits, &path).unwrap();
+}
+
+impl App {
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+        if let Some((x, y)) = self.cursor_pos.take() {
+            frame.set_cursor_position(Position::new(x, y));
+        }
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        if !self.edit_tag {
+            let has_shift = key_event.modifiers.contains(KeyModifiers::SHIFT);
+            match key_event.code {
+                KeyCode::Char('q') => self.exit(),
+                KeyCode::Char('j') | KeyCode::Char('J') => self.next_index(has_shift),
+                KeyCode::Char('k') | KeyCode::Char('K') => self.prev_index(has_shift),
+                KeyCode::Char('o') => self.open_url(),
+                KeyCode::Char('+') => self.update_state(State::Accepted),
+                KeyCode::Char('-') => self.update_state(State::Ignored),
+                KeyCode::Char(' ') => self.unroll = !self.unroll,
+                KeyCode::Char('t') => self.open_tag_editor(),
+                _ => {}
+            }
+        } else {
+            match key_event.code {
+                KeyCode::Enter => self.commit_tag(true),
+                KeyCode::Char(to_insert) => self.enter_char(to_insert),
+                KeyCode::Backspace => self.delete_char(),
+                KeyCode::Left => self.move_cursor_left(),
+                KeyCode::Right => self.move_cursor_right(),
+                KeyCode::Esc => self.commit_tag(false),
+                _ => {}
+            }
+        }
+    }
+
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();        
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            // Method "remove" is not used on the saved text for deleting the selected char.
+            // Reason: Using remove on String works on bytes instead of the chars.
+            // Using remove would require special care because of char boundaries.
+
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn open_tag_editor(&mut self) {
+        self.edit_tag = true;
+        self.input = self.commits[self.index].label.clone();
+        self.character_index = self.input.chars().count();
+    }
+
+    fn commit_tag(&mut self, commit: bool) {
+        self.edit_tag = false;
+        if commit {
+            self.commits[self.index].label = std::mem::take(&mut self.input);
+        }
+    }
+
+    fn next_index(&mut self, shift: bool) {
+        let original = self.index;
+        loop {
+            self.index += 1;
+            if self.index >= self.commits.len() {
+                self.index = 0;
+            }
+            if self.index == original || !shift {
+                break;
+            }
+            if self.commits[self.index].state == State::Untriaged {
+                break;
+            }
+        }
+    }
+
+    fn prev_index(&mut self, shift: bool) {
+        let original = self.index;
+        loop {
+            if self.index == 0 {
+                self.index = self.commits.len() - 1;
+            } else {
+                self.index -= 1;
+            }
+            if self.index == original || !shift {
+                break;
+            }
+            if self.commits[self.index].state == State::Untriaged {
+                break;
+            }
+        }
+    }
+
+    fn update_state(&mut self, state: State) {
+        self.commits[self.index].state = state;
+        self.index += 1;
+    }
+
+    fn open_url(&self) {
+        Command::new("open")
+            .arg(&self.commits[self.index].url)
+            .spawn()
+            .unwrap();
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let remaining = self.commits.iter().filter(|commit| commit.state == State::Untriaged).count();
+        let title = Line::from(format!(" Commit triage: {}/{} remaining", remaining, self.commits.len()).bold());
+        let instructions = Line::from(vec![
+            " Next ".into(),
+            "<J>".blue().bold(),
+            " Previous ".into(),
+            "<K>".blue().bold(),
+            " Accept ".into(),
+            "<+>".blue().bold(),
+            " Ignore ".into(),
+            "<->".blue().bold(),
+            " Label ".into(),
+            "<T>".blue().bold(),
+            " Open ".into(),
+            "<O>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .padding(Padding::proportional(1))
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let commit = &self.commits[self.index];
+        let title = commit.title.clone();
+        let title = match commit.state {
+            State::Ignored => title.dark_gray(),
+            State::Accepted => title.green(),
+            State::Untriaged => title.yellow(),
+        };
+        let mut lines = vec![
+            title,
+            commit.authors.join(", ").into(),
+            commit.label.as_str().white(),
+            "".into(),
+        ];
+        if self.unroll {
+            lines.extend(commit.body.iter().map(|line| line.into()));
+        } else {
+            lines.push("<space> to show body".dark_gray());
+        }
+        lines.push("".into());
+        lines.push(commit.date.split("T").next().unwrap().into());
+        let commit_text = Text::from(lines.into_iter().map(Line::from).collect::<Vec<_>>());
+
+        let input_area = block.inner(area)
+            .centered(Constraint::Percentage(50), Constraint::Length(3));
+
+        Paragraph::new(commit_text)
+            .alignment(Alignment::Left)
+            .block(block)
+            .render(area, buf);
+
+        if self.edit_tag {
+            let input_block = Block::bordered()
+                .title(Line::from("Edit label").centered())
+                .border_set(border::THICK);
+
+            Paragraph::new(self.input.as_str())
+                .alignment(Alignment::Left)
+                .block(input_block)
+                .render(input_area, buf);
+
+            self.cursor_pos.set(Some((
+                // Draw the cursor at the current position in the input field.
+                // This position is can be controlled via the left and right arrow key
+                input_area.x + self.character_index as u16 + 1,
+                // Move one line down, from the border to the input line
+                input_area.y + 1,
+            )));
+        }
+    }
+}

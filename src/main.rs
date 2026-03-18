eedc3d150331d95f7b805abab8a6429091e741a8
delan azabani <dazabani@igalia.com>
2026-03-18 17:53:25 +0800

    Rework tag editor to use byte indices internally

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ pub struct App {
     /// Current value of the input box
     input: String,
     /// Position of cursor in the editor area.
-    character_index: usize,
+    byte_index: usize,
     cursor_pos: Cell<Option<(u16, u16)>>,
 }
 
@@ -49,7 +49,7 @@ fn main() {
         unroll: false,
         exit: false,
         input: String::new(),
-        character_index: 0,
+        byte_index: 0,
         cursor_pos: Cell::new(None),
     };
     ratatui::run(|terminal| app.run(terminal)).unwrap();
@@ -114,63 +114,36 @@ impl App {
     }
 
     fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.character_index.saturating_sub(1);
-        self.character_index = self.clamp_cursor(cursor_moved_left);
+        self.byte_index = self
+            .input
+            .floor_char_boundary(self.byte_index.saturating_sub(1));
+        self.byte_index = self.byte_index.clamp(0, self.input.len());
     }
 
     fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.character_index.saturating_add(1);
-        self.character_index = self.clamp_cursor(cursor_moved_right);
+        self.byte_index = self
+            .input
+            .ceil_char_boundary(self.byte_index.saturating_add(1));
+        self.byte_index = self.byte_index.clamp(0, self.input.len());
     }
 
     fn enter_char(&mut self, new_char: char) {
-        let index = self.byte_index();
-        self.input.insert(index, new_char);
+        self.input.insert(self.byte_index, new_char);
         self.move_cursor_right();
     }
 
-    /// Returns the byte index based on the character position.
-    ///
-    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
-    /// the byte index based on the index of the character.
-    fn byte_index(&self) -> usize {
-        self.input
-            .char_indices()
-            .map(|(i, _)| i)
-            .nth(self.character_index)
-            .unwrap_or(self.input.len())
-    }
-
     fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.character_index != 0;
+        let is_not_cursor_leftmost = self.byte_index != 0;
         if is_not_cursor_leftmost {
-            // Method "remove" is not used on the saved text for deleting the selected char.
-            // Reason: Using remove on String works on bytes instead of the chars.
-            // Using remove would require special care because of char boundaries.
-
-            let current_index = self.character_index;
-            let from_left_to_current_index = current_index - 1;
-
-            // Getting all characters before the selected character.
-            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
-            // Getting all characters after selected character.
-            let after_char_to_delete = self.input.chars().skip(current_index);
-
-            // Put all characters together except the selected one.
-            // By leaving the selected one out, it is forgotten and therefore deleted.
-            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
             self.move_cursor_left();
+            self.input.remove(self.byte_index);
         }
-    }
-
-    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
-        new_cursor_pos.clamp(0, self.input.chars().count())
     }
 
     fn open_tag_editor(&mut self) {
         self.edit_tag = true;
         self.input = self.commits[self.index].label.clone();
-        self.character_index = self.input.chars().count();
+        self.byte_index = self.input.len();
     }
 
     fn commit_tag(&mut self, commit: bool) {
@@ -311,7 +284,7 @@ impl Widget for &App {
             self.cursor_pos.set(Some((
                 // Draw the cursor at the current position in the input field.
                 // This position is can be controlled via the left and right arrow key
-                input_area.x + self.character_index as u16 + 1,
+                input_area.x + self.input[0..self.byte_index].chars().count() as u16 + 1,
                 // Move one line down, from the border to the input line
                 input_area.y + 1,
             )));
